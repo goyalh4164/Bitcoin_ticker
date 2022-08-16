ebe407d6741c91cbbf47066f3ef1269b793a2040
harshg77777 <96003201+harshg77777@users.noreply.github.com>
2022-08-16 11:54:25 +0530

    initial codes with dropdown menu

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bitcoin_ticker/price_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.lightBlue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: PriceScreen(),
+    );
+  }
+}
